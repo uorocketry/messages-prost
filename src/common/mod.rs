6b67fb9454cdc91d9c33b92defe5149dd192da0d
Fernando Nogueira <abert036@uottawa.ca>
2025-06-28 15:02:26 -0400

    Remove deprecated proto files and consolidate RecoverySensing message into all.proto. Updated related Rust modules accordingly.

--- src/common/mod.rs
+++
@@ -1,9 +0,0 @@
-// This module will contain common data types and logic.
-
-use crate::messages::Node;
-
-impl From<Node> for u16 {
-    fn from(node: Node) -> Self {
-        node as i32 as u16
-    }
-}
